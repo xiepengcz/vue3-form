763890c2fc24522f4d09f800c7e7985bbee99247
谢芃 <949026370@qq.com>
2022-06-24 00:29:32 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 配置别名
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/assets/style/mian.scss";',
+      },
+    },
+  },
 })
